7838b0a9baa387b6192c4922232b542ae325a2b3
CornersOfTheCity <18892207018@163.com>
2025-07-31 18:05:39 +0800

    feat:update swap function

--- contracts/NetXExchange.sol
+++ contracts/NetXExchange.sol
@@ -7,54 +7,32 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract NetXSwap is Ownable {
     address public netXToken;
 
-    enum ActivityState {
-        NoActivity, 
-        ZoneOneActive,
-        ZoneTwoActive,
-        ZoneThreeActive
-    }
-
-    ActivityState public currentActivity;
-
     // Zone1
     address public triasNewToken;
     address public triasOldToken;
     address public tHECOToken;
     address public tToken;
 
-    uint32 public triasRate = 1000; // 1 TRIAS = 1000 NetX
-    uint32 public tHECORate = 1000; // 1 tHECO = 1000 NetX
-    uint32 public tTokenRate = 1000; // 1 tToken = 1000 NetX
+    uint32 public triasRate = 1; // 1 TRIAS = 1 NetX
+    uint32 public tHECORate = 1; // 1 tHECO = 1 NetX
+    uint32 public tTokenRate = 1; // 1 tToken = 1 NetX
 
-    // Zone2
-    address public GrowToken;
-    address public GeonToken;
-    address public TSMToken;
-    address public BEPUToken;
+    bool public openSwap ;
 
-    uint32 public growRate = 1000; // 1 Grow = 1000 NetX
-    uint32 public geonRate = 1000; // 1 Geon = 1000 NetX
-    uint32 public tsmRate = 1000; // 1 TSM = 1000 NetX
-    uint32 public bepuRate = 1000; // 1 BEPU = 1000 NetX
-
-    //Zone3
-    address public ship;
-    address public triape;
-    address public bastet;
-
-    event ActivityChange(string beforeAcctivity, string afterActivity);
-  
     constructor(
         address _initialOwner,
         address _netXToken
     ) Ownable(_initialOwner) {
         netXToken = _netXToken;
-        currentActivity = ActivityState.NoActivity;
+        openSwap = true;
     }
 
-    // Zone1
-    function swapTriasToNetX() external {
-        require(currentActivity == ActivityState.ZoneOneActive, "Another activity is already active");
+    modifier onlyWhenSwapOpen() {
+        require(openSwap, "Swap is not open");
+        _;
+    }
+
+    function swapTriasToNetX() external onlyWhenSwapOpen {
         require(triasNewToken !=address(0), "No Token Exist On Chain");
         uint256 amount = IERC20(triasNewToken).balanceOf(msg.sender);
         require(amount > 0, "Amount must be greater than zero");
@@ -64,8 +42,7 @@ contract NetXSwap is Ownable {
         IERC20(netXToken).transfer(msg.sender, amount * triasRate / 1000);
     }
 
-    function swapOldTriasToNetX() external {
-        require(currentActivity == ActivityState.ZoneOneActive, "Another activity is already active");
+    function swapOldTriasToNetX() external onlyWhenSwapOpen{
         require(triasOldToken !=address(0), "No Token Exist On Chain");
         uint256 amount = IERC20(triasOldToken).balanceOf(msg.sender);
         require(amount > 0, "Amount must be greater than zero");
@@ -75,8 +52,7 @@ contract NetXSwap is Ownable {
         IERC20(netXToken).transfer(msg.sender, amount * triasRate / 1000);
     }
 
-    function swapTHECOToNetX() external {
-        require(currentActivity == ActivityState.ZoneOneActive, "Another activity is already active");
+    function swapTHECOToNetX() external onlyWhenSwapOpen{
         require(tHECOToken !=address(0), "No Token Exist On Chain");
         uint256 amount = IERC20(tHECOToken).balanceOf(msg.sender);
         require(amount > 0, "Amount must be greater than zero");
@@ -86,8 +62,7 @@ contract NetXSwap is Ownable {
         IERC20(netXToken).transfer(msg.sender, amount * tHECORate / 1000);
     }
 
-    function swapTTokenToNetX() external {
-        require(currentActivity == ActivityState.ZoneOneActive, "Another activity is already active");
+    function swapTTokenToNetX() external onlyWhenSwapOpen{
         require(tToken !=address(0), "No Token Exist On Chain");
         uint256 amount = IERC20(tToken).balanceOf(msg.sender);
         require(amount > 0, "Amount must be greater than zero");
@@ -95,107 +70,6 @@ contract NetXSwap is Ownable {
         IERC20(tToken).transferFrom(msg.sender, address(this), amount);
         // Transfer NetX tokens to the sender
         IERC20(netXToken).transfer(msg.sender, amount * tTokenRate / 1000);
-    }
-
-    // Zone2
-    function swapGrowToNetX() external {
-        require(currentActivity == ActivityState.ZoneTwoActive, "Another activity is already active");
-        require(GrowToken !=address(0), "No Token Exist On Chain");
-        uint256 amount = IERC20(GrowToken).balanceOf(msg.sender);
-        require(amount > 0, "Amount must be greater than zero");
-        // Transfer Grow from sender to this contract
-        IERC20(GrowToken).transferFrom(msg.sender, address(this), amount);  
-        // Transfer NetX tokens to the sender
-        IERC20(netXToken).transfer(msg.sender, amount * growRate / 1000);
-    }   
-
-    function swapGeonToNetX() external {
-        require(currentActivity == ActivityState.ZoneTwoActive, "Another activity is already active");
-        require(GeonToken !=address(0), "No Token Exist On Chain");
-        uint256 amount = IERC20(GeonToken).balanceOf(msg.sender);
-        require(amount > 0, "Amount must be greater than zero");
-        // Transfer Geon from sender to this contract
-        IERC20(GeonToken).transferFrom(msg.sender, address(this), amount);
-        // Transfer NetX tokens to the sender
-        IERC20(netXToken).transfer(msg.sender, amount * geonRate / 1000);
-    }
-
-    function swapTSMToNetX() external {
-        require(currentActivity == ActivityState.ZoneTwoActive, "Another activity is already active");
-        require(TSMToken !=address(0), "No Token Exist On Chain");
-        uint256 amount = IERC20(TSMToken).balanceOf(msg.sender);
-        require(amount > 0, "Amount must be greater than zero");
-        // Transfer TSM from sender to this contract
-        IERC20(TSMToken).transferFrom(msg.sender, address(this), amount);
-        // Transfer NetX tokens to the sender
-        IERC20(netXToken).transfer(msg.sender, amount * tsmRate / 1000);
-    }
-
-    function swapBEPUToNetX() external {
-        require(currentActivity == ActivityState.ZoneTwoActive, "Another activity is already active");
-        require(BEPUToken !=address(0), "No Token Exist On Chain");
-        uint256 amount = IERC20(BEPUToken).balanceOf(msg.sender);
-        require(amount > 0, "Amount must be greater than zero");
-        // Transfer BEPU from sender to this contract
-        IERC20(BEPUToken).transferFrom(msg.sender, address(this), amount);
-        // Transfer NetX tokens to the sender
-        IERC20(netXToken).transfer(msg.sender, amount * bepuRate / 1000);
-    }
-
-    function setZoneOneToken(
-        address _triasNewToken,
-        address _triasOldToken,
-        address _tHECOToken,
-        address _tToken,
-    ) external onlyOwner {
-        triasNewToken = _triasNewToken;
-        triasOldToken = _triasOldToken;
-        tHECOToken = _tHECOToken;
-        tToken = _tToken;
-    }
-
-    function setZoneOneRates(
-        uint32 _triasRate,
-        uint32 _tHECORate,
-        uint32 _tTokenRate
-    ) external onlyOwner {
-        triasRate = _triasRate;
-        tHECORate = _tHECORate;
-        tTokenRate = _tTokenRate;
-    }
-
-    function setZoneTwoToken(
-        address _growToken,
-        address _geonToken,
-        address _tsmToken,
-        address _bepuToken
-    ) external onlyOwner {
-        GrowToken = _growToken;
-        GeonToken = _geonToken;
-        TSMToken = _tsmToken;
-        BEPUToken = _bepuToken;
-    }
-
-    function setZoneTwoRates(
-        uint32 _growRate,
-        uint32 _geonRate,
-        uint32 _tsmRate,
-        uint32 _bepuRate
-    ) external onlyOwner {
-        growRate = _growRate;
-        geonRate = _geonRate;
-        tsmRate = _tsmRate;
-        bepuRate = _bepuRate;
-    }
-
-    function setzoneThreeToken(
-        address _ship,
-        address _triape,
-        address _bastet
-    ) external onlyOwner {
-        ship = _ship;
-        triape = _triape;
-        bastet = _bastet;
     }
 
     function setnetXToken(address _netXToken) external onlyOwner {
@@ -209,11 +83,8 @@ contract NetXSwap is Ownable {
         IERC20(_token).transfer(_receiver, balance);
     }
 
-    function setActivityState(ActivityState _newActivity) external onlyOwner {
-        string memory beforeActivity = getActivityName(currentActivity);
-        currentActivity = _newActivity;
-        string memory afterActivity = getActivityName(currentActivity);
-        emit ActivityChange(beforeActivity, afterActivity);
+    function setSwapStatus(bool _status) external onlyOwner {
+        openSwap = _status;
     }
 
 }
